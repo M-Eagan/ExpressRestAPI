c2295e9ef7b181d982669d25dbeb66979f9e198f
Megan Eagan <Megan.Eagan@gmail.com>
2018-11-23 12:39:36 -0500

    added db

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const cartItems = require('./cart-items');
+const cartItems = require('./public/cart-items');
 const app = express();
 const port = 8888;
+
+
+
 
 app.use(express.json());
 app.use(express.static("./public"));
 app.use('/cartItems', cartItems);
+// app.use('/shoppingCart', shoppingCart);
 
 
 app.listen(port, () => console.log(`Server up and running on localhost:${port}`));
+
+
+
